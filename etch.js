4ea872ed38a614a41ca1c2d903db596bf1f0d7a4
mosky <smartmosky@gmail.com>
2021-07-06 00:08:08 -0400

    Added prompt to clear button that asks for a grid size and creates it

--- etch.js
+++ etch.js
@@ -4,19 +4,28 @@ button.classList.add('button');
 grid.appendChild(button);
 button.textContent = "Clear";
 button.addEventListener('click', () => {
-    document.querySelectorAll('.box').forEach(box => {box.classList.remove('blackBox')})
+    document.querySelectorAll('.box').forEach(box => {box.remove()})
+makeGrid(prompt("Choose grid size"))
 })
-for (let i =0; i<16; i++)    
+
+function makeGrid(num){
+for (let i =0; i<num; i++)    
 {const container = document.createElement('div');
 container.classList.add('container');
 grid.appendChild(container);
-for (let i =0; i<16; i++)   {
+for (let i =0; i<num; i++)   {
     const box = document.createElement('div');
 box.classList.add('box')
 container.appendChild(box);}
+};
+
+
+
 const box = document.querySelector('.box');
 document.querySelectorAll('.box').forEach(box => {box.addEventListener('mouseenter', () => {
     box.classList.add('blackBox')
     })
 });
 };
+
+makeGrid(16);
